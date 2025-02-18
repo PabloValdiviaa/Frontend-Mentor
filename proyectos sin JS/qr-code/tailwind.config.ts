89f35221298ba42a1e530c5ff3d85f0a837cab21
Pablo Valdivia <pvaldiviamonzon@gmail.com>
2025-02-18 19:01:24 -0300

    probando

--- proyectos sin JS/qr-code/tailwind.config.ts
+++ proyectos sin JS/qr-code/tailwind.config.ts
@@ -9,6 +9,12 @@ export default {
   theme: {
     extend: {
       colors: {
+        while: "hsl(0, 0%, 100%)",
+        slate: {
+          300: "hsl(212, 45%, 89%)",
+          500: "hsl(216, 15%, 48%)",
+          900: "hsl(218, 44%, 22%)",
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
